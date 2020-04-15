4b85e52f43e7306a6d22eff59a2f6862b57a4092
AbdelI <homeworkoracle@protonmail.com>
2020-04-15 19:35:38 +0200

    initial logic written

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,26 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+  #keep track of the minimum price so far
+  current_min_price_so_far = prices[0]
+  #keep track of the maximum profit so far
+  max_profit_so_far = prices[1] - current_min_price_so_far
+  # while going though the array, we wana make sure to constantly update the two.values
+  for i in range(1,prices)
+  # once we have established which is the smallest and which is the biggest, we do the difference between the two
+
+
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
